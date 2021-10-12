9e6260f80e6ecb28fb449cc495367a15b0b2888c
phamtanphat <phatdroid94@gmail.com>
2021-10-12 21:28:43 +0700

    setup recyclerview

--- app/src/main/java/com/example/recyclerview26082021/MainActivity.java
+++ app/src/main/java/com/example/recyclerview26082021/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.recyclerview26082021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView mRcvFood;
+    FoodAdapter mFoodAdapter;
+    List<Food> mListFood;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("BBB",Category.BISTRO.getValue());
+        mRcvFood = findViewById(R.id.recyclerViewFood);
+        mListFood = Food.getFoodMock();
+
+        mFoodAdapter = new FoodAdapter(mListFood);
+
+        mRcvFood.setHasFixedSize(true);
+        mRcvFood.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        mRcvFood.setAdapter(mFoodAdapter);
     }
 }
